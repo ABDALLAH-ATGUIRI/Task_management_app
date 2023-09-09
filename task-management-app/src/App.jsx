883df2f8fa2ed35429f98da8d0b8852969dfc899
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-09-09 17:01:12 +0100

    first commit to start project react with reacdux used vite and yarn

---
+++ task-management-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center"><h1 className="text-8xl font-bold text-center p-8">Hello World</h1></div>
+  )
+}
+
+export default App
