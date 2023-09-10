eff18e6cf06c72f150bfa6bc8d553a929c51fec4
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-09-10 14:23:06 +0100

    start project react

--- task-management-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function App() {
-  return (
-    <div className="h-screen flex justify-center items-center"><h1 className="text-8xl font-bold text-center p-8">Hello World</h1></div>
-  )
-}
-
-export default App
