d61abcacd798a5793f355e1421b9f71ab8e6aa10
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-09-14 05:00:31 +0100

    add drag on drop for tasks card

--- src/components/ProgressiveColumns.jsx
+++ src/components/ProgressiveColumns.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
-import { removeProgressiveColumn, changeTitle, addTicket } from '../actions/taskActions';
+import { removeProgressiveColumn, changeTitle, addTicket, changeProgressiveOfTicket } from '../actions/taskActions';
 import { connect } from 'react-redux';
+import { useDrop } from 'react-dnd';
 import { colors, icons } from '../constants';
 import { MdCheckCircleOutline, MdMoreVert } from 'react-icons/md';
 import { TbTrash } from "react-icons/tb"
@@ -8,25 +9,38 @@ import { LuPlus } from 'react-icons/lu';
 import TaskCard from './Ticket';
 
 
-const ProgressiveColumns = ({ index, id, title, tickets, changeTitle, removeProgressiveColumn, addTicket }) => {
-    const inputTitle = useRef()
+const ProgressiveColumns = ({ index, id, title, tickets, changeTitle, removeProgressiveColumn, addTicket, changeProgressiveOfTicket }) => {
+
+    const inputTitle = useRef([])
     const [cardColor, setCardColor] = useState(colors[index])
     const [cardIcon, setCardIcon] = useState(icons[index])
     const [currentTitle, setCurrentTitle] = useState(title)
     const [showOptionCard, setShowOptionCard] = useState(true)
+
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: "TASK",
+        drop: (item) => {
+            changeProgressiveOfTicket(item, id)
+        },
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver(),
+        }),
+    }))
 
     const emptyTitle = () => {
         currentTitle == "" ? removeProgressiveColumn(id) : changeTitle(id, currentTitle)
     }
 
     const newTicket = () => {
-        addTicket({ id, ticket: { id: `${Math.random(10)}-${Math.random(10)}`, name: '', description: '' } })
+        addTicket({ parentId: id, id: `${Math.random(10)}-${Math.random(10)}`, title: '', description: '' })
     }
 
-    useEffect(() => { if (currentTitle == "") inputTitle.current.focus() }, [])
+    useEffect(() => {
+        if (currentTitle == "") inputTitle.current.focus()
+    }, [])
 
     return (
-        <div className={`relative span-1 h-full max-w-96 ${index % 2 == 0 ? "bg-gray-50" : "bg-gray-100"}`}>
+        <div className={`relative span-1 h-full max-w-96 ${index % 2 == 0 ? "bg-gray-50" : "bg-gray-100"} ${isOver ? 'bg-slate-200' : ''}`} ref={drop}>
             <span className='w-full bg-purple-400 px-6 p-3 flex items-center gap-1 shadow-b-xl text-white font-bold ' style={{ backgroundColor: cardColor }}>
                 <span className="rounded-full text-2xl">{cardIcon}</span>
                 <input
@@ -39,10 +53,13 @@ const ProgressiveColumns = ({ index, id, title, tickets, changeTitle, removeProg
                     onChange={(e) => setCurrentTitle(e.target.value)}
                     onBlur={() => emptyTitle()}
                 />
+
+                <span className={`w-10 h-7 rounded-full bg-gray-300/30 cursor-pointer flex justify-center items-center ${!tickets.length ? 'hidden' : 'block'}`}>{tickets.length}</span>
+
                 <button onClick={() => setShowOptionCard(false)}>
                     <MdMoreVert size={30} />
                 </button>
-                <div className='absolute top-20 left-40 p-4 bg-white rounded-md z-30 w-64' hidden={showOptionCard} onMouseLeave={()=>setShowOptionCard(true)}>
+                <div className='absolute top-20 left-40 p-4 bg-white rounded-md z-30 w-64' hidden={showOptionCard} onMouseLeave={() => setShowOptionCard(!showOptionCard)}>
                     <div>
                         <span className='text-gray-400 font-normal py-2'>Colors</span>
                         <div className='grid grid-cols-4 gap-4 p-2 flex-wrap'>
@@ -51,28 +68,22 @@ const ProgressiveColumns = ({ index, id, title, tickets, changeTitle, removeProg
                             }
                         </div>
                     </div>
-                    {/* <div>
-                        <span className='text-gray-400 font-normal py-2'>Icons</span>
-                        <div className='grid grid-cols-4 gap-4 p-2 flex-wrap'>
-                            {
-                                icons.map((Icon, index) => <span key={index} className='rounded-full cursor-pointer text-2xl text-black h-12 w-12' onClick={() => setCardIcon(Icon)}>{Icon}</span>)
-                            }
-                        </div>
-                    </div> */}
+
                     <div>
                         <span
                             className='text-red-500 font-medium flex gap-4 hover:bg-red-50 text-md items-center py-2 justify-center cursor-pointer shadow-sm rounded-md shadow-gray-400 mt-10 focus:shadow-inherit'
                             onClick={() => removeProgressiveColumn(id)}
                         >
                             Delete
-                            <TbTrash size-2 />
+                            <TbTrash size={20} />
                         </span>
                     </div>
                 </div>
             </span>
 
-            <div className='w-full p-4 flex flex-col justify-center items-center gap-8 z-10'>
-                {tickets?.map(({ id }) => <TaskCard key={id} />)}
+            <div className='w-full p-4 flex flex-col justify-center items-center gap-8 z-10' >
+                {tickets?.map((currentTask) => <TaskCard key={currentTask.id} currentTask={currentTask} />)}
+
                 <button className='w-10 h-10 rounded-full bg-white cursor-pointer flex justify-center items-center' onClick={() => newTicket()}>
                     <LuPlus size={20} />
                 </button>
@@ -92,11 +103,12 @@ const ProgressiveColumns = ({ index, id, title, tickets, changeTitle, removeProg
 const mapDispatchToProps = {
     changeTitle,
     removeProgressiveColumn,
-    addTicket
+    addTicket,
+    changeProgressiveOfTicket,
 };
 
 const mapStateToProps = (state) => ({
-    tasks: state.tasks
+    tasks: state.tasks,
 });
 
 
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,8 +1,24 @@
 import { useEffect, useRef, useState } from "react"
+import { removeTicket, changeTicketContent } from '../actions/taskActions';
+import { connect } from 'react-redux';
+import { useDrag } from "react-dnd";
+import { MdDragIndicator, MdOutlineRemoveCircleOutline } from "react-icons/md";
 
-const Ticket = () => {
+
+const Ticket = ({ currentTask, removeTicket, changeTicketContent }) => {
+  const inputTaskTitle = useRef()
+  const [task, setTask] = useState(currentTask);
+
+  const [{ isDragging }, drag] = useDrag(({
+    type: "TASK",
+    item: () => task,
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    })
+  }))
+
   const textareaRef = useRef(null);
-  const [task, setTask] = useState()
+
 
   const resizeTextarea = () => {
     const textarea = textareaRef.current;
@@ -12,20 +28,57 @@ const Ticket = () => {
     }
   };
 
+
+  const emptyTask = () => {
+    task.title == "" && task.description == "" ? removeTicket(task.parentId, task.id) : changeTicketContent(task.parentId, task.id, task)
+  }
+
   useEffect(() => {
     resizeTextarea();
   }, [task]);
+
+  useEffect(() => {
+    if (task.title == "" && task.description == "") inputTaskTitle.current.focus()
+  }, [])
+
   return (
-    <textarea
+    <div ref={drag} className={`w-full shadow-md ${isDragging ? 'opacity-50' : ''}`}>
+      <div className="w-full p-2 px-3 bg-gray-300 text-gray-500 rounded-t-lg cursor-grab flex justify-between items-center">
+        <span><MdDragIndicator /></span>
+        <span className="cursor-pointer" onClick={() => removeTicket(task.parentId, task.id)}><MdOutlineRemoveCircleOutline /></span>
+      </div>
+      <input
+        ref={inputTaskTitle}
+        type="text"
+        value={task?.title}
+        className={`bg-gray-100 p-4 font-semibold text-md outline-0  text-gray-600 active:border-white resize-none w-full`}
+        placeholder="Title"
+        onChange={(e) => setTask({ ...task, title: e.target.value })}
+        onBlur={() => emptyTask()}
+      />
+
+      <textarea
       ref={textareaRef}
       type="text"
-      name="colomnTitle"
-      className={`bg-white p-4 font-semibold text-md outline-0 shadow-md focus:text-black active:border-white rounded-xl resize-none w-full`}
-      id="colomnTitle"
-      value={task}
-      onChange={(e) => setTask(e.target.value)}
+        name="task"
+        className={`bg-white px-4 pt-6 pb-2 font-semibold text-md outline-0  text-gray-600 active:border-white rounded-b-lg resize-none w-full h-6`}
+        placeholder="Click to add a description"
+        id="task"
+        value={task?.description}
+        onChange={(e) => setTask({ ...task, description: e.target.value })}
     />
+    </div>
+
   )
 }
 
-export default Ticket
+const mapDispatchToProps = {
+  removeTicket,
+  changeTicketContent
+};
+
+const mapStateToProps = (state) => ({
+  tasks: state.tasks
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ticket);
